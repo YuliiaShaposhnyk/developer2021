f2903cfd9064b14ad19e6c48c1ed43b684c5216e
Yuliia Shaposhnyk <shaposhnyk.yu@gmail.com>
2021-05-20 21:08:49 +0200

    add new task

--- script.js
+++ script.js
@@ -11,12 +11,31 @@ let appDate = {
     savings: false
 };
 
-let a1 = prompt("Введите бязательную сумму расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдеться?", ''),
-    a3 = prompt("Введите бязательную сумму расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдеться?", '');
 
-appDate.expenses.a1 = a2;
-appDate.expenses.a3 = a4;
+for (let i = 0; i< 2; i++){
+    let a = prompt("Введите бязательную сумму расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдеться?", '');
 
-alert(appDate.budget / 30);
+    if((typeof(a)) === 'string'&&(typeof(a))!=null&& (typeof(b)) != null && a !='' && b != '' && a.length < 50) {
+        console.log("done");
+        appDate.expenses[a] = b;
+    }else {
+
+    }
+    
+}
+
+appDate.moneyPerDay = appDate.budget / 30;
+
+
+alert("Бюждкт на каждый день: " + appDate.moneyPerDay);
+
+if(appDate.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+}else if (appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+}else if (appDate.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+}else {
+    console.log("Произошла ошибка");
+}
